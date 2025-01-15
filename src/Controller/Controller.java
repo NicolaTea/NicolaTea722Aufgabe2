7827b6db6395b60407d66c597637b39eea0172f1
NicolaTea <teanicola2004@gmail.com>
2025-01-15 16:13:27 +0200

    sort+filter methoden

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,7 +4,9 @@ import Model.Medicament;
 import Model.Patient;
 import Repo.InMemoryRepo;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Controller {
     private final InMemoryRepo<Medicament> medicamentRepo;
@@ -15,37 +17,115 @@ public class Controller {
         this.patientRepo=patientRepo;
     }
 
+    /**
+     * add
+     * @param medicament
+     */
     public void addMedicament(Medicament medicament){
         medicamentRepo.add(medicament.getId(),medicament);
     }
 
+    /**
+     * update
+     * @param medicament
+     */
     public void updateMedicament(Medicament medicament){
         medicamentRepo.update(medicament.getId(),medicament);
     }
 
+    /**
+     * delete
+     * @param medicamentID
+     */
     public void deleteMedicament(int medicamentID){
         medicamentRepo.delete(medicamentID);
     }
 
+    /**
+     * get all
+     * @return
+     */
     public List<Medicament> getAllMedicamnet(){
         return medicamentRepo.getAll();
     }
 
 
+    /**
+     * add
+     * @param patient
+     */
     public void addPatient(Patient patient){
         patientRepo.add(patient.getId(),patient);
     }
 
+    /**
+     * update
+     * @param patient
+     */
     public void updatePatient(Patient patient){
         patientRepo.update(patient.getId(),patient);
     }
 
+    /**
+     * delete
+     * @param patientId
+     */
     public void deletePatient(int patientId){
         patientRepo.delete(patientId);
     }
 
+    /**
+     * get all patient
+     * @return
+     */
     public List<Patient> getAllPatient(){
         return patientRepo.getAll();
+    }
+
+    /**
+     * add medicamentToPatient
+     * @param patientid
+     * @param medicamentint
+     */
+    public void addMedicamentToPatient(int patientid,int medicamentint ){
+        Patient patient=patientRepo.getById(patientid);
+        Medicament medicament=medicamentRepo.getById(medicamentint);
+        if (patient != null && medicament!= null) {
+            patient.getMedicamentList().add(medicament);
+            System.out.println("Medicament '" + medicament.getName() + "' added to Patient '" + patient.getName() + "'.");
+        } else {
+            System.out.println(" does not exist.");
+        }
+    }
+
+    /**
+     * filter by diagnose
+     * @param diagnose
+     * @return
+     */
+    public List<Patient> filterByDiagnose(String diagnose){
+        return patientRepo.getAll().stream()
+                .filter(p->p.getDiagnose().equalsIgnoreCase(diagnose))
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * sortMedicament
+     * @param patientId
+     * @param ascending
+     * @return
+     */
+    public List<Medicament> sortMedicament(int patientId, boolean ascending) {
+        Patient kunde = patientRepo.getById(patientId);
+        if (kunde == null) return List.of();
+
+        return kunde.getMedicamentList().stream()
+                .sorted((p1, p2) -> {
+                    int comparison = Double.compare(p1.getPrice(), p2.getPrice());
+                    return ascending ? comparison : -comparison;
+                })
+                .collect(Collectors.toList());
     }
 
 
