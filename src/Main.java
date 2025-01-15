8426beca2ae6f1a81fa3008ec8dd898a46790cc9
NicolaTea <teanicola2004@gmail.com>
2025-01-15 16:13:55 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Controller.Controller;
+import Model.Medicament;
+import Model.Patient;
+import Repo.InMemoryRepo;
+import View.View;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        InMemoryRepo<Patient> patient=new InMemoryRepo<>();
+        InMemoryRepo<Medicament> medicament=new InMemoryRepo<>();
+         Controller controller=new Controller(medicament,patient);
+         View view=new View(controller);
+         view.start();
     }
 }
